08da625214883dd30664c5a61d61052c322fc432
nireo <eemil.lehtikangas1@gmail.com>
2019-05-15 23:09:48 +0300

    added score for user and added functionality for true and false, basically quiz feature workinggit add .

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,10 +1,42 @@
-import React from "react"
+import React, {useState} from "react"
 
 const Question = (props) => {
+    const [questionNumber, setQuestionNumber] = useState(Number(0))
+    const [score, setScore] = useState(0)
+
+    if (questionNumber === undefined) {
+        setQuestionNumber(Number(0))
+    }
+
+    console.log(questionNumber)
+    console.log(props.questions[questionNumber].correct_answer)
+
+    const handleTrue = () => {
+        if (props.questions[questionNumber].correct_answer === "True") {
+            console.log("your answer was correct")
+            setScore(score + 1)
+        } else {
+            console.log("your answer is incorrect")
+        }
+    }   
+
+    const handleFalse = () => {
+        if (props.questions[questionNumber].correct_answer === "False") {
+            console.log("your answer was correct")
+            setScore(score + 1)
+        } else {
+            console.log("your answer is incorrect")
+        }
+    }
+
     return (
     <div>
-       <h3>{props.questions[0].question}</h3> 
-       <div><button className="btn btn-primary">True</button>    <button className="btn btn-danger">False</button></div>
+       <h3>{props.questions[questionNumber].question}</h3> 
+       <div>
+            <button onClick={() => {handleTrue(); setQuestionNumber(questionNumber + 1) }} className="btn btn-primary">True</button>
+            <button onClick={() => {handleFalse(); setQuestionNumber(questionNumber + 1) }} className="btn btn-danger">False</button>
+        </div>
+       <div>Score: {score}/10</div>
     </div>)
 }
 
