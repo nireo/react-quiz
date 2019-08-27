e754a5c1f9ba0b8a178e83b459e4090b58a8f35d
nireo <eemil.lehtikangas1@gmail.com>
2019-08-27 20:26:30 +0300

    completed question rendering

--- src/components/Multiple.js
+++ src/components/Multiple.js
@@ -2,9 +2,11 @@ import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
 import { initQuestions } from '../reducers/questionReducer';
 import Loading from './Loading';
+import { Link } from 'react-router-dom';
 
 const Multiple = props => {
-  const [shuffled, setShuffled] = useState([]);
+  const [index, setIndex] = useState(0);
+  const [score, setScore] = useState(0);
   const shuffle = arr => {
     for (let i = arr.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
@@ -16,15 +18,41 @@ const Multiple = props => {
     if (props.questions === null) {
       props.initQuestions('multiple');
     }
-    if (shuffled === [] && props.questions !== null) {
-      setShuffled();
-    }
   }, [props]);
   if (props.questions === null) {
     return <Loading />;
   }
 
-  return <div className="container">loaded</div>;
+  const renderQuestions = shuffle(
+    props.questions[index].incorrect_answers.concat(
+      props.questions[index].correct_answer
+    )
+  ).map(q => (
+    <Link onClick={() => handleNextQuestion(q)} className="collection-item">
+      {q}
+    </Link>
+  ));
+
+  const handleNextQuestion = answer => {
+    if (answer === props.questions[index].correct_answer) {
+      setScore(score + 1);
+    }
+    setIndex(index + 1);
+  };
+
+  return (
+    <div className="container">
+      <h3>
+        {props.questions[index].question
+          .replace(/&quot;/g, '"')
+          .replace(/&#039;/g, "'")}
+      </h3>
+      <div>
+        <div className="collection">{renderQuestions}</div>
+      </div>
+      score: {score}
+    </div>
+  );
 };
 
 const mapStateToProps = state => {
